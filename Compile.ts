102b04c6119cf762eb0816d7260cdddab955ec50
Your Name <50252724+DrMeepso@users.noreply.github.com>
2024-12-02 16:26:15 +1300

    h

--- Compile.ts
+++ Compile.ts
@@ -36,27 +36,32 @@ if (!fs.existsSync(outputFolder)) {
 // clone the repository
 import * as child_process from 'child_process'
 
-console.log("Cloning WebFishingCove repository...")
-child_process.spawnSync("git", ["clone", "https://github.com/DrMeepso/WebFishingCove.git"])
-console.log("Cloned WebFishingCove repository")
+(async () => {
+    console.log("Cloning WebFishingCove repository...")
+    const git_process = child_process.spawnSync("git", ["clone", "https://github.com/DrMeepso/WebFishingCove.git"])
+    git_process.stdout && console.log(git_process.stdout.toString())
+    console.log("Cloned WebFishingCove repository")
 
-// compile each project
+    await new Promise((resolve) => setTimeout(resolve, 1000))
 
-Projects.forEach(project => {
-    const projectPath = project.projectFolder + "/" + project.projectName + ".csproj"
-    const dllPath = project.projectFolder + "/bin/Debug/net8.0/" + project.dllName
-    const outputPath = outputFolder + "/" + project.dllName
+    // compile each project
 
-    console.log(`Compiling ${project.projectName}`)
+    Projects.forEach(async (project) => {
+        const projectPath = project.projectFolder + "/" + project.projectName + ".csproj"
+        const dllPath = project.projectFolder + "/bin/Debug/net8.0/" + project.dllName
+        const outputPath = outputFolder + "/" + project.dllName
 
-    // spawn a new process to compile the project
-    child_process.spawnSync("dotnet", ["restore", projectPath])
-    const compileProcess = child_process.spawnSync("dotnet", ["build", projectPath])
-    // print the output of the compilation
-    compileProcess.stdout && console.log(compileProcess.stdout.toString())
-    fs.copyFileSync(dllPath, outputPath)
+        console.log(`Compiling ${project.projectName}`)
 
-    console.log(`Compiled ${project.projectName}`)
-})
+        // spawn a new process to compile the project
+        child_process.spawnSync("dotnet", ["restore", projectPath])
+        const compileProcess = child_process.spawnSync("dotnet", ["build", projectPath])
+        // print the output of the compilation
+        compileProcess.stdout && console.log(compileProcess.stdout.toString())
+        fs.copyFileSync(dllPath, outputPath)
 
-console.log("Compilation complete")
+        console.log(`Compiled ${project.projectName}`)
+    })
+
+    console.log("Compilation complete")
+})();
